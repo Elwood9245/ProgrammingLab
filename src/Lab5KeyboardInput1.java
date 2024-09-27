83e3c8c845e56ac24ebed55b75464161b22859b6
Elwood <elwood@glaroam2-171-82.wireless.gla.ac.uk>
2024-09-27 12:20:55 +0100

    240927 Lab5 finished.

---
+++ src/Lab5KeyboardInput1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Lab5KeyboardInput {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter your name, followed by the return key.");
+        String userEntry = keyboard.nextLine();
+        System.out.println("Hello " + userEntry);
+        System.out.println("Please enter your age");
+        int userAge = keyboard.nextInt();
+        keyboard.nextLine();
+        System.out.println("You are " + userAge + " years old");
+        System.out.println("What is your favourite colour?");
+        String col = keyboard.nextLine();
+    }
+
+}
---
+++ src/Lab5KeyboardInput5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Lab5KeyboardInput2 {
+
+    public static void summariseCircle(double radius) {
+        double circumference = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("For a circle with radius: " + radius);
+        System.out.println("Circumference: " + circumference);
+        System.out.println("Area: " + area);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the radius of the circle: ");
+        double radius = scanner.nextDouble();
+        summariseCircle(radius);
+        scanner.close();
+    }
+}
