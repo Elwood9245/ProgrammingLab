ce2b49ac2af2f3bad7fdf4afb237549b7fdcecbf
Elwood <elwoodwjz@outlook.com>
2024-10-01 13:20:42 +0100

    Week 2 Lab begin

--- src/Person.java
+++ src/Person.java
@@ -3,9 +3,25 @@ public class Person {
     private int age;
     private  String name;
 
-    public Person(String s, int a){
-        age = a;
-        name = s;
+    public Person(String name, int age){
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public void sayHello(){
--- src/Students.java
+++ src/Students.java
@@ -1,5 +1,28 @@
 public class Students extends Person{
 
-    
+    private double studentGPA;
 
+    public Students(String name, int age, double studentGPA){
+        super(name, age);
+        this.studentGPA = studentGPA;
+    }
+
+    public double getStudentGPA() {
+        return studentGPA;
+    }
+
+    public void setStudentGPA(double studentGPA) {
+        this.studentGPA = studentGPA;
+    }
+
+    @Override
+    public void sayHello(){
+        System.out.println("Hi. My name is " + getName() + ". And my GPA is " + getStudentGPA());
+
+    }
+
+    public static void main(String[] args) {
+        Students student = new Students("Elwood", 23, 3.2);
+        student.sayHello();
+    }
 }
